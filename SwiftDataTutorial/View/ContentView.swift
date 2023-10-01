d5b732c045400e98c97d63c299e86128f0fc176d
Asif Syeed <asifsyeed666@gmail.com>
2023-10-01 22:20:01 +0600

    Delete data

--- SwiftDataTutorial/View/ContentView.swift
+++ SwiftDataTutorial/View/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftData
 
 struct ContentView: View {
     
+    @Environment(\.modelContext) var context
     @State private var isShowingItemSheet = false
     @Query(sort: \Expense.date) var expenses: [Expense]
 
@@ -18,6 +19,11 @@ struct ContentView: View {
             List {
                 ForEach(expenses) { expense in
                     ExpenseCell(expense: expense)
+                }
+                .onDelete { indexSet in
+                    for index in indexSet {
+                        context.delete(expenses[index])
+                    }
                 }
             }
             .navigationTitle("Expenses")
